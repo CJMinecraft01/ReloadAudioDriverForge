62c2d792242e1b8ad5538e3ba9f464a57aa9e8ef
Jonas Lateur <jonas@lateur.pro>
2019-06-17 03:20:53 +0200

    Updated to forge 1.14.2

--- src/main/java/cjminecraft/rad/ReloadAudioDriver.java
+++ src/main/java/cjminecraft/rad/ReloadAudioDriver.java
@@ -2,44 +2,54 @@ package cjminecraft.rad;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.I18n;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.client.util.InputMappings;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
+
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.lwjgl.input.Keyboard;
+
 
 /**
  * A simple mod which will reload the audio drivers when pressing F3 + R
  */
-@Mod(modid = ReloadAudioDriver.MODID, name = ReloadAudioDriver.NAME, version = ReloadAudioDriver.VERSION, clientSideOnly = true)
+@Mod(ReloadAudioDriver.MODID)
 @Mod.EventBusSubscriber
-public class ReloadAudioDriver {
-
+public class ReloadAudioDriver
+{
     static final String MODID = "rad";
     static final String NAME = "Reload Audio Driver";
     static final String VERSION = "${version}";
     private static Logger LOGGER = LogManager.getFormatterLogger(ReloadAudioDriver.NAME);
 
     @SubscribeEvent
-    @SideOnly(Side.CLIENT)
-    public static void onKeyInputEvent(InputEvent.KeyInputEvent event) {
-        if (Keyboard.isKeyDown(Keyboard.KEY_F3)) {
-            if (Keyboard.isKeyDown(Keyboard.KEY_R)) {
-                Minecraft.getMinecraft().actionKeyF3 = true;
+    public static void onKeyInputEvent(InputEvent.KeyInputEvent event)
+    {
+        // keys: https://www.glfw.org/docs/latest/group__keys.html
+        if (InputMappings.isKeyDown(Minecraft.getInstance().mainWindow.getHandle(),292))
+        {
+            if (InputMappings.isKeyDown(Minecraft.getInstance().mainWindow.getHandle(), 82))
+            {
+                Minecraft.getInstance().keyboardListener.actionKeyF3 = true;
                 LOGGER.info("Reloading sounds!");
-                Minecraft.getMinecraft().getSoundHandler().sndManager.reloadSoundSystem();
+                Minecraft.getInstance().getSoundHandler().sndManager.reload();
                 LOGGER.info("Reloaded sounds!");
-                Minecraft.getMinecraft().player.sendMessage(new TextComponentString(TextFormatting.GREEN + I18n.format("reload_audio_driver.success")));
-            } else if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
-                Minecraft.getMinecraft().player.sendMessage(new TextComponentTranslation("reload_audio_driver.details"));
+                //Hardcoded for now
+                //Minecraft.getInstance().player.sendMessage((new StringTextComponent("")).appendSibling((new TranslationTextComponent("reload_audio_driver.success")).applyTextStyles(new TextFormatting[]{TextFormatting.GREEN})));
+                Minecraft.getInstance().player.sendMessage((new StringTextComponent("Successfully reloaded audio drivers!")).applyTextStyles(new TextFormatting[]{TextFormatting.GREEN}));
+
+            }
+            else if (InputMappings.isKeyDown(Minecraft.getInstance().mainWindow.getHandle(), 81))
+            {
+                //Hardcoded for now F3 + R = Reload audio drivers
+                //Minecraft.getInstance().player.sendMessage((new StringTextComponent("")).appendSibling((new TranslationTextComponent("reload_audio_driver.details"))));
+                Minecraft.getInstance().player.sendMessage(new StringTextComponent("F3 + R = Reload audio drivers"));
             }
         }
     }
-
 }
