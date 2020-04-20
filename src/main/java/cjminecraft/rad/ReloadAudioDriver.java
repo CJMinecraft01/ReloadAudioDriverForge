72f61b5909d079fb8a50a31e2641ec72a77a6d73
CJMinecraft01 <speed2pc2@gmail.com>
2020-04-20 11:40:04 +0100

    Updated to 1.14

--- src/main/java/cjminecraft/rad/ReloadAudioDriver.java
+++ src/main/java/cjminecraft/rad/ReloadAudioDriver.java
@@ -1,43 +1,43 @@
 package cjminecraft.rad;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.I18n;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextComponentTranslation;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.client.util.InputMappings;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.lwjgl.input.Keyboard;
+import org.lwjgl.glfw.GLFW;
 
 /**
  * A simple mod which will reload the audio drivers when pressing F3 + R
  */
-@Mod(modid = ReloadAudioDriver.MODID, name = ReloadAudioDriver.NAME, version = ReloadAudioDriver.VERSION, clientSideOnly = true)
+@Mod(ReloadAudioDriver.MODID)
 @Mod.EventBusSubscriber
 public class ReloadAudioDriver {
 
-    static final String MODID = "rad";
-    static final String NAME = "Reload Audio Driver";
-    static final String VERSION = "${version}";
-    private static Logger LOGGER = LogManager.getFormatterLogger(ReloadAudioDriver.NAME);
+    public static final String MODID = "rad";
+    private static final Logger LOGGER = LogManager.getLogger(MODID);
 
     @SubscribeEvent
-    @SideOnly(Side.CLIENT)
+    @OnlyIn(Dist.CLIENT)
     public static void onKeyInputEvent(InputEvent.KeyInputEvent event) {
-        if (Keyboard.isKeyDown(Keyboard.KEY_F3)) {
-            if (Keyboard.isKeyDown(Keyboard.KEY_R)) {
-                Minecraft.getMinecraft().actionKeyF3 = true;
-                LOGGER.info("Reloading sounds!");
-                Minecraft.getMinecraft().getSoundHandler().sndManager.reloadSoundSystem();
-                LOGGER.info("Reloaded sounds!");
-                Minecraft.getMinecraft().player.sendMessage(new TextComponentString(TextFormatting.GREEN + I18n.format("reload_audio_driver.success")));
-            } else if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
-                Minecraft.getMinecraft().player.sendMessage(new TextComponentTranslation("reload_audio_driver.details"));
+        if (event.getAction() != GLFW.GLFW_PRESS)
+            return;
+        if (Minecraft.getInstance().currentScreen == null) {
+            if (InputMappings.isKeyDown(Minecraft.getInstance().mainWindow.getHandle(), GLFW.GLFW_KEY_F3)) {
+                if (event.getKey() == GLFW.GLFW_KEY_R) {
+                    Minecraft.getInstance().keyboardListener.actionKeyF3 = true;
+                    LOGGER.info("Reloading sounds!");
+                    Minecraft.getInstance().getSoundHandler().sndManager.reload();
+                    LOGGER.info("Reloaded sounds!");
+                    Minecraft.getInstance().keyboardListener.printDebugMessage("reload_audio_driver.success");
+                } else if (event.getKey() == GLFW.GLFW_KEY_Q) {
+                    Minecraft.getInstance().ingameGUI.getChatGUI().printChatMessage(new TranslationTextComponent("reload_audio_driver.details"));
+                }
             }
         }
     }
